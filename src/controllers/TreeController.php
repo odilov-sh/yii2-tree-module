e3c90c7b36e1120bd8ffb09f1add77531c75d011
Shukurullo <shukurullo0321@gmail.com>
2020-10-03 16:09:16 +0500

    Started

--- src/controllers/TreeController.php
+++ src/controllers/TreeController.php
@@ -4,6 +4,7 @@
 namespace odilov\treemodule\controllers;
 
 
+use yii\db\Query;
 use yii\helpers\Json;
 use odilov\treemodule\models\Tree;
 use Yii;
@@ -11,40 +12,46 @@ use Yii;
 class TreeController extends \yii\web\Controller
 {
 
-    public function actionSave()
-    {
-        return "salom";
-    }
+    public $enableCsrfValidation = false;
 
     public function actionSaveTree()
     {
-        $this->enableCsrfValidation = false;
         $tree = Yii::$app->request->post('tree');
+        $tableName = Yii::$app->request->post('tableName');
+        $query = new Query();
 
         foreach ($tree as $t){
 
-            $model = Tree::findOne($t['id']);
+            $id = $t['id'];
+            $model = $query->select('*')
+                ->from($tableName)
+                ->where(['id' => $id])
+                ->one();
+
             if ($model != null){
-
-                $model->sort = $t['order'];
-                $parentId = $t['parentId'];
-                $model->parent_id = $parentId;
-
-                if ($parentId != null){
-
-                    $parent = Tree::findOne($parentId);
-                    if ($parent){
-                        $model->level = $parent->level + 1;
+                $sort = $t['order'];
+                $level = $model['level'];
+                $parentId = isset($t['parentId']) ? $t['parentId'] :  '';
+                if ($parentId != ''){
+                    $parent = $query->select('*')
+                        ->from($tableName)
+                        ->where(['id' => $parentId])
+                        ->one();
+                    if ($parent != null){
+                        $level = $parent['level'] + 1;
                     }
-
                 }
 
-                $model->save();
+                $connection = Yii::$app->db;
+
+                $connection->createCommand()->update($tableName, [
+                    'sort' => $sort,
+                    'parent_id' => $parentId,
+                    'level' => $level,
+
+                ], 'id ='.$id)->execute();
 
             }
-
         }
-
     }
-
 }
--- src/widget/TreeWidget.php
+++ src/widget/TreeWidget.php
@@ -3,30 +3,21 @@
 
 namespace odilov\treemodule\widget;
 
-
+use Yii;
 use odilov\treemodule\assets\TreeManagerAssset;
 use yii\base\Widget;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
-class TreeManager extends Widget
+class TreeWidget extends Widget
 {
 
-
-    public $options = [];
-
-    public $listContainerOptions = [];
-
     public $query;
-
-    public function init()
-    {
-        parent::init();
-
-        if (!ArrayHelper::keyExists('id', $this->options )){
-            ArrayHelper::setValue($this->options, 'id', $this->getId());
-        }
-    }
+    public $options = [];
+    public $listContainerOptions = [];
+    public $saveButtonOptions = [];
+    public $actionColumn = [];
 
     public function run()
     {
@@ -36,30 +27,28 @@ class TreeManager extends Widget
     public function renderTree()
     {
         $tag = ArrayHelper::remove($this->options, 'tag', 'div');
+        ArrayHelper::setValue($this->options, 'id', $this->getId());
         Html::addCssClass($this->options, 'tree-div');
         $list = $this->renderList();
-
         $this->registerAssets();
+        $saveButton = $this->renderSaveButton();
+        $content = $list . $saveButton;
 
-        return Html::tag($tag, $list, $this->options);
+        return Html::tag($tag, $content, $this->options);
 
     }
 
     public function renderList()
     {
-        $listContainerTag = ArrayHelper::remove($this->listContainerOptions, 'tag', 'ul');
-        $id = ArrayHelper::getValue($this->options, 'id');
-        $listId = $id."-tree";
 
-        if (!ArrayHelper::keyExists('id', $this->listContainerOptions )){
+        if (!ArrayHelper::keyExists('id', $this->listContainerOptions)) {
+            $listId = $this->id . "-tree";
             ArrayHelper::setValue($this->listContainerOptions, 'id', $listId);
         }
 
         Html::addCssClass($this->listContainerOptions, 'tree-ul');
-
         $items = $this->renderItems();
-
-        return Html::tag($listContainerTag, $items, $this->listContainerOptions);
+        return Html::tag('ul', $items, $this->listContainerOptions);
     }
 
     public function renderItems()
@@ -75,22 +64,75 @@ class TreeManager extends Widget
     public function renderItem($item)
     {
 
-        $name = Html::tag('div', $item->name);
+
+        $action = Yii::createObject([
+            'class' => \yii\grid\ActionColumn::className(),
+        ]);
+
+        $actionColumn = Html::tag('span', $action->renderDataCell($item, $item->id, 1) , [
+            'class' => 'pull-right action-column'
+        ]);
+
+        $name = Html::tag('div', $item->name.$actionColumn);
 
         $modelClass = $this->getModelClass();
         $childs = '';
-        $childItems = $modelClass::find()->where(['parent_id' => $item->id])->all();
+        $childItems = $modelClass::find()->where(['parent_id' => $item->id])->orderBy(['sort' => SORT_ASC])->all();
 
         if (!empty($childItems)) {
 
-            foreach ($item->childs as $child) {
+            foreach ($childItems as $child) {
                 $childs .= $this->renderItem($child);
             }
             $childs = Html::tag('ul', $childs, ['class' => 'tree-child-list-wrap']);
         }
 
-        return Html::tag('li', $name . $childs, ['id' => $item->id]);
+        return Html::tag('li', $name  . $childs, ['id' => $item->id]);
 
+    }
+
+    public function renderActionColumn()
+    {
+
+    }
+
+    public function renderSaveButton()
+    {
+        $label = ArrayHelper::remove($this->saveButtonOptions, 'label', Yii::t('app', 'Save'));
+        if (!isset($this->saveButtonOptions['class'])) {
+            Html::addCssClass($this->saveButtonOptions, 'btn btn-success');
+        }
+
+        Html::addCssClass($this->saveButtonOptions, 'save-tree-button');
+
+        if (!isset($this->saveButtonOptions['id'])) {
+
+            $saveButtonId = $this->id . "-save-button";
+            $this->saveButtonOptions['id'] = $saveButtonId;
+        }
+
+        $url = Url::to(['/treemodule/tree/save-tree']);
+        $tableName = $this->getTableName();
+        $treeId = $this->listContainerOptions['id'];
+
+        $js = "
+                $('#{$this->saveButtonOptions['id']}').click(function(e){
+                e.preventDefault()
+                var tree =  $('#{$this->listContainerOptions['id']}').sortableListsToArray()
+                var url = '{$url}'
+                var tableName = '{$tableName}'
+                $.ajax({
+                    url: url,
+                    data: {
+                        'tree' : tree,
+                        'tableName' : tableName,
+                    },
+                    type: 'post',
+                })
+            })
+        ";
+        $this->getView()->registerJs($js);
+        return Html::button($label, $this->saveButtonOptions);
     }
 
     public function registerAssets()
@@ -156,7 +198,7 @@ class TreeManager extends Widget
                 }
             };
 
-            $('#".$listId."').sortableLists( options );
+            $('#" . $listId . "').sortableLists( options );
         });
 ";
 
@@ -167,6 +209,12 @@ class TreeManager extends Widget
     private function getModelClass()
     {
         return $this->query->modelClass;
+    }
+
+    private function getTableName()
+    {
+        $modelClass = $this->getModelClass();
+        return $modelClass::tableName();
     }
 
 }
